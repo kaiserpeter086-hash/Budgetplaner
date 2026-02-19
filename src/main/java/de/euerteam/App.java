05018e1d046657fae5e59dbd71a03a32aa09bdcf
danielaustrierrrrr <daniel.c14@gso.schule.koeln>
2026-02-19 09:38:15 +0100

    Initial Maven project (Java 17)

---
+++ src/main/java/de/euerteam/App.java
@@ -0,0 +1,13 @@
+package de.euerteam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
